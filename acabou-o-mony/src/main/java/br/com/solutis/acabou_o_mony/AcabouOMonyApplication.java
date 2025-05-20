6c3faf95d9b37ad313e111cab7de1b1b5256b1ca
raulreis123 <raulgomesreis@gmail.com>
2025-05-20 13:58:06 -0300

    feature: subindo estrutura do projeto

---
+++ acabou-o-mony/src/main/java/br/com/solutis/acabou_o_mony/AcabouOMonyApplication.java
@@ -0,0 +1,13 @@
+package br.com.solutis.acabou_o_mony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcabouOMonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcabouOMonyApplication.class, args);
+	}
+
+}
---
+++ acabou-o-mony/src/test/java/br/com/solutis/acabou_o_mony/AcabouOMonyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solutis.acabou_o_mony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcabouOMonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
