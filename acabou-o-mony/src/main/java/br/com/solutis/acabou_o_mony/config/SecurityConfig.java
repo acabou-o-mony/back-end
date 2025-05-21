a682a126cf18ee9efbb8ebf53bb16cb4a3abdeb6
raulreis123 <raulgomesreis@gmail.com>
2025-05-21 15:57:22 -0300

    feature: Configuração do spring security (configuração default)

---
+++ acabou-o-mony/src/main/java/br/com/solutis/acabou_o_mony/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package br.com.solutis.acabou_o_mony.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Desativa CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Libera tudo
+                );
+
+        return http.build();
+    }
+}
