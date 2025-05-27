8ae5a982ac26eb4938005ae5121bc5836972c41c
ENZO JORGE SOARES NASCIMENTO <enzo.nascimento@solutis.net.br>
2025-05-27 10:26:19 -0300

    feature: Estrutura de microserviço de transações

---
+++ transacao-service/src/main/java/br/com/solutis/transacao_service/TransacaoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.solutis.transacao_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoServiceApplication.class, args);
+	}
+
+}
---
+++ transacao-service/src/test/java/br/com/solutis/transacao_service/TransacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solutis.transacao_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
