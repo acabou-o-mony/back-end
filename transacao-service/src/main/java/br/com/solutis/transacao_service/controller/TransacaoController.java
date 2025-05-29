6cdc7a78772dd022d4493459a626e804dda8df7d
ENZO JORGE SOARES NASCIMENTO <enzo.nascimento@solutis.net.br>
2025-05-29 09:47:57 -0300

    feature: Criação de métoodo de atualização de transação

--- transacao-service/src/main/java/br/com/solutis/transacao_service/controller/TransacaoController.java
+++ transacao-service/src/main/java/br/com/solutis/transacao_service/controller/TransacaoController.java
@@ -2,16 +2,14 @@ package br.com.solutis.transacao_service.controller;
 
 import br.com.solutis.transacao_service.dto.TransacaoRequestDto;
 import br.com.solutis.transacao_service.dto.TransacaoResponseDto;
+import br.com.solutis.transacao_service.entity.Transacao;
 import br.com.solutis.transacao_service.mapper.TransacaoMapper;
 import br.com.solutis.transacao_service.service.TransacaoService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -33,4 +31,13 @@ public class TransacaoController {
         return ResponseEntity.status(201).body(mapper.toResponse(service.novaTransacao(req)));
     }
 
+    // TODO
+    @PutMapping("/paga/{id}")
+    public ResponseEntity<TransacaoResponseDto> atualizarTransacao(@PathVariable Long id, @RequestBody boolean paga) {
+        if (id == null) return ResponseEntity.status(400).build();
+
+        Transacao entity = service.atualizarTransacao(id, paga);
+
+        return (entity == null) ? ResponseEntity.status(404).build() : ResponseEntity.status(200).body(mapper.toResponse(entity));
+    }
 }
