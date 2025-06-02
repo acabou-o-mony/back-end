9dacde3fe6ebbcc35aa609daae3799327e2cc929
raulreis123 <raulgomesreis@gmail.com>
2025-06-02 14:32:54 -0300

    feature: Finalização de configuração do JWT

--- usuario-service/src/main/java/br/com/solutis/usuario_service/config/SecurityConfig.java
+++ usuario-service/src/main/java/br/com/solutis/usuario_service/config/SecurityConfig.java
@@ -1,21 +1,130 @@
 package br.com.solutis.usuario_service.config;
 
+import br.com.solutis.usuario_service.config.jwtConfig.AutenticacaoEntryPoint;
+import br.com.solutis.usuario_service.config.jwtConfig.AutenticacaoFilter;
+import br.com.solutis.usuario_service.config.jwtConfig.AutenticacaoProvider;
+import br.com.solutis.usuario_service.config.jwtConfig.GerenciadorTokenJwt;
+import br.com.solutis.usuario_service.service.AutenticacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+    @Autowired
+    private AuthenticationEntryPoint authenticacaoJwtEntryPoint;
+
+    private static final String[] URLS_PERMITIDAS = {
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/api/public/**",
+            "/api/public/authenticate",
+            "/webjars/**",
+            "/v3/api-docs/**",
+            "/actuator/*",
+            "/usuarios/cadastro",
+            "/usuarios/"
+    };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Desativa CSRF
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Libera tudo
-                );
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .cors(Customizer.withDefaults())
+                .csrf(CsrfConfigurer<HttpSecurity>::disable)
+                .authorizeHttpRequests(request -> request
+                    .requestMatchers(URLS_PERMITIDAS).permitAll()
+                    .anyRequest().authenticated())
+                .exceptionHandling(handling -> handling
+                        .authenticationEntryPoint(authenticacaoJwtEntryPoint))
+                .sessionManagement(management -> management
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(jwtAuthenticationFilterBean(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder.authenticationProvider(new AutenticacaoProvider(autenticacaoService, passwordEncoder()));
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public AutenticacaoEntryPoint jwtAuthenticationEntryPoint(){ return new AutenticacaoEntryPoint(); }
+
+    @Bean
+    public AutenticacaoFilter jwtAuthenticationFilterBean(){
+        return new AutenticacaoFilter(autenticacaoService, jwtAuthenticationUtilBean());
+    }
+
+    @Bean
+    public GerenciadorTokenJwt jwtAuthenticationUtilBean(){
+        return new GerenciadorTokenJwt();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration cors = new CorsConfiguration();
+
+        cors.applyPermitDefaultValues();
+        cors.setAllowedMethods(
+                Arrays.asList(
+                        HttpMethod.POST.name(),
+                        HttpMethod.GET.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.TRACE.name()
+                )
+        );
+
+        cors.setExposedHeaders(List.of(HttpHeaders.CONTENT_DISPOSITION));
+
+        UrlBasedCorsConfigurationSource origem = new UrlBasedCorsConfigurationSource();
+        origem.registerCorsConfiguration("/**", cors);
+
+        return origem;
     }
 }
