958e14daf19cacbe92bdcb46b636496a4f92aff3
ENZO JORGE SOARES NASCIMENTO <enzo.nascimento@solutis.net.br>
2025-06-03 11:19:11 -0300

    feature: Criação da api gateway do projeto com a controller pronta

---
+++ api-gateway/src/main/java/br/com/solutis/api_gateway/controller/GatewayController.java
@@ -0,0 +1,53 @@
+package br.com.solutis.api_gateway.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api")
+public class GatewayController {
+
+    private final WebClient client;
+
+    public GatewayController() {
+        this.client = WebClient.builder().build();
+    }
+
+    @RequestMapping("/{service}/{path:^(?!api).*$}/**")
+    public Mono<ResponseEntity<String>> proxy(
+            @PathVariable String service,
+            @PathVariable String path,
+            @RequestHeader HttpHeaders headers,
+            @RequestParam(required = false) MultiValueMap<String, String> queryParams,
+            @RequestBody(required = false) Mono<String> body,
+            ServerHttpRequest request
+            ) {
+
+        String baseUrl = switch (service) {
+            case "contas" -> "http://localhost:8080";
+            case "usuarios" -> "http://localhost:8080";
+            case "produtos" -> "http://localhost:8081";
+            case "pedidos" -> "http://localhost:8085";
+            case "carrinhos" -> "http://localhost:8085";
+            case "transacoes" -> "http://localhost:8083";
+            default -> null;
+        };
+
+        if (baseUrl == null) return Mono.just(ResponseEntity.status(400).body("Serviço não encontrado."));
+
+        String fullPath = request.getURI().getRawPath().replace("/api/" + service, "");
+
+        return client.method(request.getMethod())
+                .uri(baseUrl + fullPath)
+                .headers(httpHeaders -> httpHeaders.addAll(headers))
+                .body(body == null ? Mono.empty() : body, String.class)
+                .retrieve()
+                .toEntity(String.class);
+    }
+
+}
---
+++ api-gateway/src/main/java/br/com/solutis/api_gateway/filter/AuthWebFilter.java
@@ -0,0 +1,4 @@
+package br.com.solutis.api_gateway.filter;
+
+public class AuthWebFilter {
+}
